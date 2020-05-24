2113e85584ba726d9786b877686988637afc7ff3
Bogi <@>
2020-05-24 23:28:56 +0200

    Add session handling

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from wtform_fields import *
 from models import *
 from passlib.hash import pbkdf2_sha256
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 
 app = Flask(__name__)
 app.secret_key = 'replace later'
@@ -11,6 +12,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/C
 
 db = SQLAlchemy(app)
 
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -36,8 +43,25 @@ def login():
 
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        return 'Logged in!'
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        return redirect(url_for('chat'))
+
     return render_template('login.html', form=login_form)
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+
+    if not current_user.is_authenticated:
+        return 'Please login before accessing chat'
+
+    return 'Chat with me!!'
+
+@app.route('/logout', methods=['GET'])
+def logout():
+
+    logout_user()
+    return 'Logged out using flask-login!'
 
 if __name__ == "__main__":
 
